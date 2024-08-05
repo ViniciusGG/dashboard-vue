e69e412c1f776390d28b22568ea729bb7239b70a
ViniciusGG <vinicius1663.vg@gmail.com>
2024-08-04 23:28:51 -0300

    new components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,27 +15,21 @@ import logo from '@/assets/img/logo.png'
           <div class="md:ml-auto">
             <div class="flex space-x-2">
               <RouterLink to="/" :class="[
-                isActiveLink('/')
-                  ? 'bg-green-900'
-                  : 'hover:bg-gray-900 hover:text-white',
+              'hover:bg-gray-900 hover:text-white',
                 'text-white',
                 'px-3',
                 'py-2',
                 'rounded-md',
               ]">Home</RouterLink>
               <RouterLink to="/jobs" :class="[
-                isActiveLink('/jobs')
-                  ? 'bg-green-900'
-                  : 'hover:bg-gray-900 hover:text-white',
+                'hover:bg-gray-900 hover:text-white',
                 'text-white',
                 'px-3',
                 'py-2',
                 'rounded-md',
               ]">Jobs</RouterLink>
               <RouterLink to="/jobs/add" :class="[
-                isActiveLink('/jobs/add')
-                  ? 'bg-green-900'
-                  : 'hover:bg-gray-900 hover:text-white',
+                'hover:bg-gray-900 hover:text-white',
                 'text-white',
                 'px-3',
                 'py-2',
